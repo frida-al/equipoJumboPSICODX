ba1226669130e105b34d03a98031eca80f770fa4
Nicole Uriostegui <142357118+nicoleum17@users.noreply.github.com>
2025-04-14 17:07:52 -0600

    KOSTICK MODELS

--- preguntasKostick.model.js
+++ preguntasKostick.model.js
@@ -1,28 +1,41 @@
 const db = require("../util/database.js");
 
 module.exports = class PreguntaKostick {
-  constructor(mi_preguntaKostick, mi_idPrueba, mi_numeroPreguntaKostick) {
+  constructor(mi_idPrueba, mi_numeroPreguntaKostick) {
     this.idPreguntaKostick = uuidv4();
-    this.preguntaKostick = mi_preguntaKostick;
     this.idPrueba = mi_idPrueba;
     this.numeroPreguntaKostick = mi_numeroPreguntaKostick;
   }
 
   static fetchAll() {
-    return db.execute("SELECT * FROM preguntasKostick");
+    return db.execute("SELECT * FROM preguntaskostick");
   }
 
   static fetchOne(index) {
     return db.execute(
-      "SELECT * FROM preguntasKostick WHERE numeroPreguntaKostick = ?",
+      "SELECT * FROM preguntaskostick WHERE numeroPreguntaKostick = ?",
       [index]
     );
   }
 
   static getOpciones(idPreguntaKostick) {
     return db.execute(
-      "SELECT * FROM opcionesKostick WHERE idPreguntaKostick = ?",
+      "SELECT * FROM opcioneskostick WHERE idPreguntaKostick = ?",
       [idPreguntaKostick]
     );
   }
+
+  /*static fetchOne(idPreguntaKostick) {
+    return db.execute(
+      "SELECT * FROM preguntasKostick WHERE idPreguntaKostick = ?",
+      [idPreguntaKostick]
+    );
+  }
+
+  static fetchOneByNumber(numeroPreguntaKostick) {
+    return db.execute(
+      "SELECT * FROM preguntasKostick WHERE numeroPreguntaKostick = ?",
+      [numeroPreguntaKostick]
+    );
+  }*/
 };
