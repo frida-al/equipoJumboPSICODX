c61c42da0f0bdd9ec4464d7e5d1266ff1a5fbb68
Nicole Uriostegui <a01711853@tec.mx>
2025-04-22 18:08:56 -0600

    Lo separé

---
+++ SoftSync/models/preguntasKostick.model.js
@@ -0,0 +1,41 @@
+const db = require("../util/database.js");
+
+module.exports = class PreguntaKostick {
+  constructor(mi_idPrueba, mi_numeroPreguntaKostick) {
+    this.idPreguntaKostick = uuidv4();
+    this.idPrueba = mi_idPrueba;
+    this.numeroPreguntaKostick = mi_numeroPreguntaKostick;
+  }
+
+  static fetchAll() {
+    return db.execute("SELECT * FROM preguntaskostick");
+  }
+
+  static fetchOne(index) {
+    return db.execute(
+      "SELECT * FROM preguntaskostick WHERE numeroPreguntaKostick = ?",
+      [index]
+    );
+  }
+
+  static getOpciones(idPreguntaKostick) {
+    return db.execute(
+      "SELECT * FROM opcioneskostick WHERE idPreguntaKostick = ?",
+      [idPreguntaKostick]
+    );
+  }
+
+  /*static fetchOne(idPreguntaKostick) {
+    return db.execute(
+      "SELECT * FROM preguntasKostick WHERE idPreguntaKostick = ?",
+      [idPreguntaKostick]
+    );
+  }
+
+  static fetchOneByNumber(numeroPreguntaKostick) {
+    return db.execute(
+      "SELECT * FROM preguntasKostick WHERE numeroPreguntaKostick = ?",
+      [numeroPreguntaKostick]
+    );
+  }*/
+};
---
+++ SoftSync/models/respondeKostick.model.js
@@ -0,0 +1,48 @@
+const db = require("../util/database");
+const bcrypt = require("bcryptjs");
+const { v4: uuidv4 } = require("uuid");
+
+module.exports = class RespondeKostick {
+  constructor(
+    mi_idPreguntaKostick,
+    mi_idGrupo,
+    mi_idUsuario,
+    mi_idOpcionKostick,
+    mi_tiempo
+  ) {
+    this.idRespuestaKostick = uuidv4();
+    this.idPreguntaKostick = mi_idPreguntaKostick;
+    this.idGrupo = mi_idGrupo;
+    this.idUsuario = mi_idUsuario;
+    this.idOpcionKostick = mi_idOpcionKostick;
+    this.tiempo = mi_tiempo;
+  }
+  save() {
+    return db
+      .execute(
+        "INSERT INTO respondekostick (idRespuestaKostick, idPreguntaKostick, idGrupo, idUsuario, idOpcionKostick, tiempo) VALUES (?,?,?,?,?,?)",
+        [
+          this.idRespuestaKostick,
+          this.idPreguntaKostick,
+          this.idGrupo,
+          this.idUsuario,
+          this.idOpcionKostick,
+          this.tiempo,
+        ]
+      )
+      .then(([result]) => {
+        return {
+          idPreguntaKostick: this.idPreguntaKostick,
+          idGrupo: this.idGrupo,
+          idUsuario: this.idUsuario,
+        };
+      });
+  }
+
+  static fetchRespuesta(idGrupo, idUsuario, numeroPreguntaKostick) {
+    return db.execute(
+      "SELECT opcionKostick FROM opcioneskostick WHERE idOpcionKostick = (SELECT idOpcionKostick FROM respondekostick WHERE idGrupo = ? AND idUsuario = ? AND idPreguntaKostick = (SELECT idPreguntaKostick FROM preguntaskostick WHERE numeroPreguntaKostick = ?))",
+      [idGrupo, idUsuario, numeroPreguntaKostick]
+    );
+  }
+};
