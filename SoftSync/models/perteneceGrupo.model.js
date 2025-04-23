2260b4fde5ac9a2fd78b042e8a2246ffbaace3e2
Frida 3 <149642064+frida-al@users.noreply.github.com>
2025-04-23 17:17:38 -0600

    Create perteneceGrupo.model.js

---
+++ SoftSync/models/perteneceGrupo.model.js
@@ -0,0 +1,41 @@
+const db = require("../util/database");
+const bcrypt = require("bcryptjs");
+const { v4: uuidv4 } = require("uuid");
+
+module.exports = class PerteneceGrupo {
+  constructor(mi_idGrupo, mi_idUsuario, mi_fechaZoomIndividual, mi_enlaceZoom) {
+    this.idGrupo = mi_idGrupo;
+    this.idUsuario = mi_idUsuario;
+    this.fechaZoomIndividual = mi_fechaZoomIndividual;
+    this.enlaceZoom = mi_enlaceZoom;
+  }
+
+  save() {
+    return db.execute(
+      "INSERT INTO pertenecegrupo (idGrupo, idUsuario, fechaZoomIndividual, enlaceZoom) VALUES (?, ?, ?, ?)",
+      [this.idGrupo, this.idUsuario, this.fechaZoomIndividual, this.enlaceZoom]
+    );
+  }
+
+  static fetchAll() {
+    return db.execute("SELECT * FROM pertenecegrupo");
+  }
+  static fetchOne(idUsuario) {
+    return db.execute("SELECT * FROM pertenecegrupo WHERE idUsuario = ?", [
+      idUsuario,
+    ]);
+  }
+
+  static updatePerteneceGrupo() {
+    return db.execute(
+      "UPDATE pertenecegrupo SET fechaZoomIndividual = ?, enlaceZoom = ? WHERE idUsuario = ?",
+      [fechaZoomIndividual, enlaceZoom, idUsuario]
+    );
+  }
+
+  static consultarReporte(idUsuario) {
+    return db.execute("SELECT * FROM perteneceGrupo WHERE idUsuario = ?", [
+      idUsuario,
+    ]);
+  }
+};
